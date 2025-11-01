3869eaaf75278a795736faf8a98fb1798a148c21
Willow Otto <pz8313ru@go.minnsate.edu>
2025-11-01 15:36:48 -0500

    Added camel.py code

---
+++ camel.py
@@ -0,0 +1,17 @@
+def camelcase(sentence):
+    """ Convert a sentence to camelCase, for example Display all books
+    is converted to displayAllBooks. """
+    title_case = sentence.title() #Uppercase first letter of each word
+    upper_camel_cased = title_case.replace(" ", "") #Remove spaces
+    # Lovercase first letter, join with rest of string
+    #Slices dont produce index out of bounds errors.
+    #So this still works on empty strings, strings of length 1
+    return upper_camel_cased[0].lower() + upper_camel_cased[1:]
+
+def main():
+    sentence = input("Enter a sentence:")
+    output = camelcase(sentence)
+    print(output)
+
+if __name__ == "__main__":
+    main()
